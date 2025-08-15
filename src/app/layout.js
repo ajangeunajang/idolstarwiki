203685346531ef2d5991f07edff4385a79ec7771
장은아 <aa@Eunas-MacBookPro.local>
2025-08-15 22:08:31 +0900

    og+ favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,26 +1,26 @@
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import { Geist, Geist_Mono } from 'next/font/google';
+import './globals.css';
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
 });
 
 export const metadata = {
-  title: "IdolstarWiki",
-  description: "아이돌스타위키",
+  title: 'IdolstarWiki',
+  description: '아이돌스타위키',
   openGraph: {
     images: [
       {
-        url: "/2_og.png",
+        url: '/og.png',
         width: 1200,
         height: 630,
-        alt: "아이돌스타위키",
+        alt: '아이돌스타위키',
       },
     ],
   },
